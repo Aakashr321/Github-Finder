b14164dcdc03d8780e01a455942e887398258a5f
Aakash Rathore <aakash.rathore321@gmail.com>
2022-08-20 17:34:14 +0530

    Created single api call using axios for get user and repos

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,23 +1,20 @@
+import axios from "axios";
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+});
 
 //Get Search Results
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({ q: text });
 
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    method: "GET",
-    //   headers: {
-    // Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    //   },
-  });
-  const { items } = await response.json();
-  // setUsers(data);
-  // setLoading(false);
-
-  return items;
+  const response = await github.get(`/search/users?${params}`);
+  return response.data.items;
 };
 
-//Get Single User
+/* //Get Single User
 export const getUser = async (login) => {
   const response = await fetch(`${GITHUB_URL}/users/${login}`, {
     method: "GET",
@@ -49,4 +46,15 @@ export const getUserRepos = async (login) => {
   const data = await response.json();
  
   return data
+}; */
+
+//Updated Get user and repos
+
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ]);
+
+  return { user: user.data, repos: repos.data };
 };
