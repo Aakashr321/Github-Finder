fed7adcd819ab05bca7606570e2dead382e280eb
Aakash Rathore <aakash.rathore321@gmail.com>
2022-08-20 17:55:31 +0530

    refactored new update

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,60 +1,28 @@
-import axios from "axios";
-
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+import axios from 'axios'
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-});
+  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+})
 
-//Get Search Results
+// Get search results
 export const searchUsers = async (text) => {
-  const params = new URLSearchParams({ q: text });
+  const params = new URLSearchParams({
+    q: text,
+  })
 
-  const response = await github.get(`/search/users?${params}`);
-  return response.data.items;
-};
+  const response = await github.get(`/search/users?${params}`)
+  return response.data.items
+}
 
-/* //Get Single User
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-    method: "GET",
-    //   headers: {
-    // Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    //   },
-  });
-
-  if (response.status === 404) {
-    window.location = "/not found";
-  } else {
-    const data = await response.json();
-  
-    return data
-  
-  }
-};
-
-//Get User Repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({ sort: "created", per_page: 10 });
-
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    method: "GET",
-    //   headers: {
-    // Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    //   },
-  });
-  const data = await response.json();
- 
-  return data
-}; */
-
-//Updated Get user and repos
-
+// Get user and repos
 export const getUserAndRepos = async (login) => {
   const [user, repos] = await Promise.all([
     github.get(`/users/${login}`),
     github.get(`/users/${login}/repos`),
-  ]);
+  ])
 
-  return { user: user.data, repos: repos.data };
-};
+  return { user: user.data, repos: repos.data }
+}
